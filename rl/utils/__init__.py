3d0edeef6d43b4facb50e202b9b6d319abee9c19
seungju-mmc <ksj970213@hanyang.ac.kr>
2023-09-04 15:12:28 +0900

    Refactoring repo.

--- rl/utils/__init__.py
+++ rl/utils/__init__.py
@@ -1,18 +1,3 @@
-# MAKINAROCKS CONFIDENTIAL
-# ________________________
-#
-# [2017] - [2023] MakinaRocks Co., Ltd.
-# All Rights Reserved.
-#
-# NOTICE:  All information contained herein is, and remains
-# the property of MakinaRocks Co., Ltd. and its suppliers, if any.
-# The intellectual and technical concepts contained herein are
-# proprietary to MakinaRocks Co., Ltd. and its suppliers and may be
-# covered by U.S. and Foreign Patents, patents in process, and
-# are protected by trade secret or copyright law. Dissemination
-# of this information or reproduction of this material is
-# strictly forbidden unless prior written permission is obtained
-# from MakinaRocks Co., Ltd.
 """Utility Code."""
 from .miscellaneous import convert_dict_as_param, setup_logger, clamp
 
