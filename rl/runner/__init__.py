1466a66fb7a123e0ffa3510c51420a27ccf7e856
seungju <ksj970213@hanynag.ac.kr>
2023-12-06 20:31:18 +0900

    Runner: Control RL Algorithms.

--- rl/runner/__init__.py
+++ rl/runner/__init__.py
@@ -1,4 +1,4 @@
 from .run import run_rl
-from .run_w_checkpoint import run_rl_w_checkpoint
+from .run_w_checkpoint import run_rl_w_ckpt
 
-__all__ = ["run_rl", "run_rl_w_checkpoint"]
+__all__ = ["run_rl", "run_rl_w_ckpt"]
