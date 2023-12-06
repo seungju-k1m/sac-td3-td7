725ea7d2efab018a8730253bf430b9977d3b02ab
seungju <ksj970213@hanynag.ac.kr>
2023-12-06 20:30:01 +0900

    Base Replayer buffer.

--- rl/replay_buffer/simple.py
+++ rl/replay_buffer/simple.py
@@ -5,7 +5,7 @@ from typing import Any
 import numpy as np
 import torch
 from rl.replay_buffer.base import BaseReplayBuffer
-from rl.utils.annotation import ACTION, BATCH, DONE, OBSERVATION, REWARD
+from rl.utils.annotation import ACTION, BATCH, DONE, STATE, REWARD
 
 
 class SimpleReplayBuffer(BaseReplayBuffer):
@@ -18,26 +18,28 @@ class SimpleReplayBuffer(BaseReplayBuffer):
     def sample(self, batch_size: int, use_torch: bool = True) -> BATCH:
         """Sample."""
         transitions = random.sample(self.queue, batch_size)
-        obs: OBSERVATION = np.stack(list(map(lambda x: x[0], transitions)), 0)
+        state: STATE = np.stack(list(map(lambda x: x[0], transitions)), 0)
         action: ACTION = np.stack(list(map(lambda x: x[1], transitions)), 0)
         reward: REWARD = (
             np.array(list(map(lambda x: x[2], transitions)))
             .astype(np.float32)
             .reshape(-1, 1)
         )
-        next_obs: OBSERVATION = np.stack(list(map(lambda x: x[3], transitions)), 0)
+        next_state: STATE = np.stack(list(map(lambda x: x[3], transitions)), 0)
         done: DONE = (
             np.array(list(map(lambda x: x[4], transitions)))
             .astype(np.float32)
             .reshape(-1, 1)
         )
         if use_torch:
-            obs = torch.Tensor(obs)
+            state = torch.Tensor(state)
             action = torch.Tensor(action)
             reward = torch.Tensor(reward)
-            next_obs = torch.Tensor(next_obs)
+            next_state = torch.Tensor(next_state)
             done = torch.Tensor(done)
-        return dict(obs=obs, action=action, reward=reward, next_obs=next_obs, done=done)
+        return dict(
+            state=state, action=action, reward=reward, next_state=next_state, done=done
+        )
 
     def append(self, transition: list[Any]) -> None:
         """Append."""
