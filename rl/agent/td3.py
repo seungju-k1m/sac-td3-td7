f0a66a2f3867a7b776cb81463fd0f1f2bf293950
seungju <ksj970213@hanynag.ac.kr>
2023-12-06 20:29:05 +0900

    TD3 Algorithm.

--- rl/agent/td3.py
+++ rl/agent/td3.py
@@ -12,33 +12,31 @@ import gymnasium as gym
 from torch import nn
 
 from rl import SAVE_DIR
-from rl.agent.base import Agent
+from rl.agent.abc import Agent
 from rl.replay_buffer.lap import LAPReplayBuffer
 from rl.replay_buffer.simple import SimpleReplayBuffer
 from rl.sampler import Sampler
-from rl.neural_network import (
-    calculate_grad_norm,
-    clip_grad_norm,
-    make_feedforward,
+from rl.neural_network import calculate_grad_norm, MLPPolicy, MLPQ
+from rl.utils.annotation import ACTION, BATCH, DONE, STATE, REWARD
+from rl.utils.miscellaneous import (
+    convert_dict_as_param,
+    get_state_action_dims,
+    setup_logger,
 )
-from rl.utils.annotation import ACTION, BATCH, DONE, OBSERVATION, REWARD
-from rl.utils.miscellaneous import convert_dict_as_param, setup_logger
-from rl.runner import run_rl, run_rl_w_checkpoint
+from rl.runner import run_rl
 
 
 class TD3(Agent, Sampler):
-    """Agent for Soft Actor Critic."""
+    """Agent for TD3."""
 
     def __init__(
         self,
-        action_space: gym.spaces.Dict,
-        obs_space: gym.spaces.Box,
+        env_id: str,
         discount_factor: float = 0.99,
         hidden_sizes: list[int] = [256, 256],
         action_fn: str | nn.Module = "ReLU",
         policy_lr: float = 3e-4,
         critic_lr: float = 3e-4,
-        target_update_rate: int = 250,
         exploration_noise: float = 0.1,
         target_policy_noise: float = 0.2,
         noise_clip: float = 0.5,
@@ -47,41 +45,48 @@ class TD3(Agent, Sampler):
         use_lap: bool = False,
         **kwargs,
     ) -> None:
-        self.discount_factor = discount_factor
-        self.hidden_sizes = hidden_sizes
-        self.action_fn = action_fn
-        self.obs_space = obs_space
-        self.action_space = action_space
-        self.action_dim = action_space.shape[-1]
+        """Initialize."""
+        assert env_id in gym.registry
+        # Make neural network.
+        state_dim, action_dim = get_state_action_dims(env_id)
+        self.policy = MLPPolicy(state_dim, action_dim, hidden_sizes, action_fn)
+        self.q1 = MLPQ(state_dim, action_dim, hidden_sizes, action_fn)
+        self.q2 = MLPQ(state_dim, action_dim, hidden_sizes, action_fn)
+        self.target_policy = deepcopy(self.policy)
+        self.target_q1, self.target_q2 = deepcopy(self.q1), deepcopy(self.q2)
 
-        self.target_update_rate = target_update_rate
+        # Save cconiguration.
+        self.discount_factor = discount_factor
         self.target_policy_noise = target_policy_noise
         self.exploration_noise = exploration_noise
         self.noise_clip = noise_clip
         self.policy_freq = policy_freq
-        self.tau = tau
-        self.make_nn()
-        self.make_optimizers(policy_lr, critic_lr)
-        self.n_runs = 0
         self.use_lap = use_lap
+        self.tau = tau
+        self.n_runs = 0
+        self.device = torch.device("cpu")
+        self.to(self.device)
 
-    def make_nn(self) -> None:
-        """Make neurla networks."""
-        obs_dim = self.obs_space.shape[-1]
-        self.policy = make_feedforward(
-            obs_dim, self.action_dim, self.hidden_sizes, self.action_fn
-        )
+        # Make optimizer.
+        self.make_optimizers(policy_lr, critic_lr)
 
-        self.target_policy = deepcopy(self.policy)
+    def to(self, device: torch.device) -> None:
+        """Attach device."""
+        self.policy = self.policy.to(device)
+        self.target_policy = self.policy.to(device)
+        self.q1 = self.q1.to(device)
+        self.q2 = self.q2.to(device)
+        self.target_q1 = self.target_q1.to(device)
+        self.target_q2 = self.target_q2.to(device)
+        self.device = device
 
-        self.q1 = make_feedforward(
-            obs_dim + self.action_dim, 1, self.hidden_sizes, self.action_fn
-        )
-        self.q2 = make_feedforward(
-            obs_dim + self.action_dim, 1, self.hidden_sizes, self.action_fn
-        )
-        self.target_q1 = deepcopy(self.q1)
-        self.target_q2 = deepcopy(self.q2)
+    def load_state_dict(self, agent: "TD3") -> None:
+        """Load state dict."""
+        self.q1.load_state_dict(agent.q1.state_dict())
+        self.q2.load_state_dict(agent.q2.state_dict())
+        self.policy.load_state_dict(agent.policy.state_dict())
+        self.target_q1.load_state_dict(agent.target_q1.state_dict())
+        self.target_q2.load_state_dict(agent.target_q2.state_dict())
 
     def make_optimizers(self, policy_lr: float, critic_lr: float) -> None:
         """Make optimizers."""
@@ -95,14 +100,27 @@ class TD3(Agent, Sampler):
         self.optim_q_fns.zero_grad()
         self.optim_policy.zero_grad()
 
-    def step(self) -> None:
-        """Step backpropgagtion via optimizers."""
-        self.optim_q_fns.step()
-        self.optim_policy.step()
+    def sample(self, state: STATE, deterministic: bool = False, **kwargs) -> ACTION:
+        """Sample action."""
+        with torch.no_grad():
+            if not isinstance(state, torch.Tensor):
+                state = torch.Tensor(state).float()
+            if state.ndim == 1:
+                state = state.unsqueeze(0)
+            state = state.to(self.device)
+            action = self._inference_action(state)
+            action = action.cpu().detach().numpy()[0]
+            if not deterministic:
+                noise = np.random.normal(
+                    0, 1.0 * self.exploration_noise, size=action.shape
+                )
+                action += noise
+                action = np.clip(action, -1.0, 1.0)
+        return action
 
-    def policy_forward(self, obs: OBSERVATION) -> ACTION:
+    def _inference_action(self, state: STATE) -> ACTION:
         """Only forward with policy."""
-        mean = self.policy.forward(obs)
+        mean = self.policy.forward(state)
         action = torch.tanh(mean)
         return action
 
@@ -113,11 +131,11 @@ class TD3(Agent, Sampler):
             td_error < min_priority, 0.5 * td_error.pow(2), min_priority * td_error
         ).mean()
 
-    def _q_value_ops(
+    def _q_train_ops(
         self,
-        obs: OBSERVATION,
+        state: STATE,
         action: REWARD,
-        next_obs: OBSERVATION,
+        next_state: STATE,
         reward: REWARD,
         done: DONE,
     ) -> torch.Tensor | tuple[torch.Tensor, torch.Tensor]:
@@ -126,18 +144,16 @@ class TD3(Agent, Sampler):
             noise = (torch.rand_like(action) * self.target_policy_noise).clamp(
                 -self.noise_clip, self.noise_clip
             )
-            next_action = (torch.tanh(self.target_policy(next_obs)) + noise).clamp(
+            next_action = (torch.tanh(self.target_policy(next_state)) + noise).clamp(
                 -1.0, 1.0
             )
-            next_obs_action = torch.cat((next_obs, next_action), -1)
             next_value = torch.min(
-                self.target_q1.forward(next_obs_action),
-                self.target_q2.forward(next_obs_action),
+                self.target_q1.forward(next_state, next_action),
+                self.target_q2.forward(next_state, next_action),
             )
             q_target = reward + self.discount_factor * next_value * done
         # calculate q value
-        obs_action = torch.cat((obs, action), 1)
-        q1, q2 = self.q1.forward(obs_action), self.q2.forward(obs_action)
+        q1, q2 = self.q1.forward(state, action), self.q2.forward(state, action)
         if self.use_lap:
             td_loss1 = (q1 - q_target).abs()
             td_loss2 = (q2 - q_target).abs()
@@ -153,34 +169,16 @@ class TD3(Agent, Sampler):
             q_loss = q1_loss + q2_loss
             return q_loss
 
-    def _policy_ops(self, obs: OBSERVATION, **kwargs) -> torch.Tensor:
+    def _policy_train_ops(self, state: STATE, **kwargs) -> torch.Tensor:
         """Policy ops."""
         # Calculate policy loss.
-        action = self.policy_forward(obs)
-        obs_action = torch.cat((obs, action), -1)
-        q1, q2 = self.q1.forward(obs_action), self.q2.forward(obs_action)
+        action = self._inference_action(state)
+        q1, q2 = self.q1.forward(state, action), self.q2.forward(state, action)
         policy_loss = -torch.min(q1, q2).mean()
         return policy_loss
 
-    def sample(self, obs: OBSERVATION, deterministic: bool = False, **kwargs) -> ACTION:
-        """Sample action for inference action."""
-        with torch.no_grad():
-            if not isinstance(obs, torch.Tensor):
-                obs = torch.Tensor(obs).float()
-            if obs.ndim == 1:
-                obs = obs.unsqueeze(0)
-            action = self.policy_forward(obs)
-            action = action.cpu().detach().numpy()[0]
-            if not deterministic:
-                noise = np.random.normal(
-                    0, 1.0 * self.exploration_noise, size=action.shape
-                )
-                action += noise
-                action = np.clip(action, -1.0, 1.0)
-        return action
-
     @torch.no_grad()
-    def update_target_value_fn(self) -> None:
+    def _update_target_q_fns(self) -> None:
         """Update target value function."""
         for q_parm, t_q_parm in zip(self.q1.parameters(), self.target_q1.parameters()):
             t_q_parm.copy_(self.tau * q_parm + t_q_parm * (1 - self.tau))
@@ -199,39 +197,35 @@ class TD3(Agent, Sampler):
         **kwargs,
     ) -> None:
         """Run train ops."""
-        # Update state value function.
         info = {}
-        max_norm = float("inf")
+        batch = {key: value.to(self.device) for key, value in batch.items()}
+
         # Update Action-State value function.
-        q_value_loss = self._q_value_ops(**batch)
+        q_value_loss = self._q_train_ops(**batch)
         if isinstance(q_value_loss, tuple):
             q_value_loss, priority = q_value_loss
+            assert isinstance(replay_buffer, LAPReplayBuffer)
+            replay_buffer.update_priority(priority)
         q_value_loss.backward()
         info["norm/q1_value"] = calculate_grad_norm(self.q1)
         info["norm/q2_value"] = calculate_grad_norm(self.q2)
-        clip_grad_norm(self.q1, max_norm)
-        clip_grad_norm(self.q2, max_norm)
         self.optim_q_fns.step()
         self.zero_grad()
-        info["loss/q_value"] = float(q_value_loss.cpu().detach().numpy())
+        info["train/q_fn"] = float(q_value_loss.cpu().detach().numpy())
 
         # Update policy.
-        info["loss/policy"] = None
+        info["train/policy"] = None
         info["norm/policy"] = None
-        if self.use_lap:
-            assert isinstance(replay_buffer, LAPReplayBuffer)
-            replay_buffer.update_priority(priority)
         if self.n_runs % self.policy_freq == 0:
-            policy_loss = self._policy_ops(**batch)
+            policy_loss = self._policy_train_ops(**batch)
             policy_loss.backward()
-            info["loss/policy"] = float(policy_loss.cpu().detach().numpy())
+            info["train/policy"] = float(policy_loss.cpu().detach().numpy())
             info["norm/policy"] = calculate_grad_norm(self.policy)
-            clip_grad_norm(self.policy, max_norm)
 
             self.optim_policy.step()
             self.zero_grad()
             # Update Target
-            self.update_target_value_fn()
+            self._update_target_q_fns()
         self.n_runs += 1
         return info
 
@@ -240,12 +234,11 @@ def run_td3(
     rl_run_name: str,
     env_id: str,
     seed: int = 777,
-    action_fn: str = "ReLU",
-    discount_factor: float = 0.99,
-    use_checkpoint: bool = False,
     use_lap: bool = False,
     replay_buffer_size: int = 1_000_000,
     benchmark_idx: int = 0,
+    record_video: bool = False,
+    show_progressbar: bool = True,
     **kwargs,
 ) -> None:
     """Run Heating Environment."""
@@ -260,13 +253,15 @@ def run_td3(
             / "VALID"
             / env_id
             / "TD3"
-            / f"SAC-{rl_run_name}"
+            / f"TD3-{rl_run_name}"
             / str(benchmark_idx)
         )
+        show_progressbar = False
+        record_video = False
     else:
         timestamp = datetime.strftime(datetime.now(), "%Y-%m-%d-%H:%M:%S")
         rl_run_name = f"{rl_run_name}-{timestamp}"
-        base_dir = SAVE_DIR / "SAC" / rl_run_name
+        base_dir = SAVE_DIR / "TD3" / rl_run_name
     base_dir.mkdir(exist_ok=True, parents=True)
 
     # TODO: Replace logger by mlflow.
@@ -283,10 +278,7 @@ def run_td3(
     # Make envs
     env = gym.make(env_id)
     agent = TD3(
-        env.action_space,
-        env.observation_space,
-        action_fn=action_fn,
-        discount_factor=discount_factor,
+        env_id,
         use_lap=use_lap,
         **kwargs,
     )
@@ -297,7 +289,12 @@ def run_td3(
             replay_buffer_size, env.observation_space, env.action_space
         )
     )
-    if use_checkpoint:
-        run_rl_w_checkpoint(env, agent, logger, base_dir, replay_buffer, **kwargs)
-    else:
-        run_rl(env, agent, logger, base_dir, replay_buffer, **kwargs)
+    run_rl(
+        env,
+        agent,
+        replay_buffer,
+        logger,
+        show_progressbar=show_progressbar,
+        record_video=record_video,
+        **kwargs,
+    )
