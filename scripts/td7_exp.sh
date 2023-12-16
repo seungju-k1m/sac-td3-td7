0afb5ab7f872d8f49a02df576f6d5ae48dc0bc66
seungju <ksj970213@hanynag.ac.kr>
2023-12-16 12:36:10 +0900

    Add experiment scripts.

--- scripts/td7_exp.sh
+++ scripts/td7_exp.sh
@@ -1,8 +1,8 @@
-python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed111 --n-iteration 1000000  --seed 111&&
-python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed222 --n-iteration 1000000  --seed 222&&
-python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed333 --n-iteration 1000000  --seed 333&&
-python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed444 --n-iteration 1000000  --seed 444&&
-python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed555 --n-iteration 1000000  --seed 555&&
-python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed666 --n-iteration 1000000  --seed 666&&
-python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed777 --n-iteration 1000000  --seed 777&&
-python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed888 --n-iteration 1000000  --seed 888&&
+python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed111 --n-iteration 1000000  --seed 111 --use-gpu&&
+python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed222 --n-iteration 1000000  --seed 222 --use-gpu&&
+python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed333 --n-iteration 1000000  --seed 333 --use-gpu&&
+python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed444 --n-iteration 1000000  --seed 444 --use-gpu&&
+python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed555 --n-iteration 1000000  --seed 555 --use-gpu&&
+python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed666 --n-iteration 1000000  --seed 666 --use-gpu&&
+python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed777 --n-iteration 1000000  --seed 777 --use-gpu&&
+python cli.py rl td7 --env-id HalfCheetah-v4 --run-name cheetah@seed888 --n-iteration 1000000  --seed 888 --use-gpu&&
