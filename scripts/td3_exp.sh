b9c637ec4046fe7ee810a07fb0b4e0ef736f7ac8
seungju <ksj970213@hanynag.ac.kr>
2023-12-16 12:30:58 +0900

    Add experiment scripts.

---
+++ scripts/td3_exp.sh
@@ -0,0 +1,8 @@
+python cli.py rl td3 --env-id HalfCheetah-v4 --run-name cheetah@seed111 --n-iteration 1000000  --seed 111&&
+python cli.py rl td3 --env-id HalfCheetah-v4 --run-name cheetah@seed222 --n-iteration 1000000  --seed 222&&
+python cli.py rl td3 --env-id HalfCheetah-v4 --run-name cheetah@seed333 --n-iteration 1000000  --seed 333&&
+python cli.py rl td3 --env-id HalfCheetah-v4 --run-name cheetah@seed444 --n-iteration 1000000  --seed 444&&
+python cli.py rl td3 --env-id HalfCheetah-v4 --run-name cheetah@seed555 --n-iteration 1000000  --seed 555&&
+python cli.py rl td3 --env-id HalfCheetah-v4 --run-name cheetah@seed666 --n-iteration 1000000  --seed 666&&
+python cli.py rl td3 --env-id HalfCheetah-v4 --run-name cheetah@seed777 --n-iteration 1000000  --seed 777&&
+python cli.py rl td3 --env-id HalfCheetah-v4 --run-name cheetah@seed888 --n-iteration 1000000  --seed 888&&
