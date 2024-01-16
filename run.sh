018964868d45ed744304a326cbe7b93ad525f1c4
seungju <ksj970213@hanynag.ac.kr>
2024-01-16 13:02:51 +0900

    Run file for reproducible experiment.

--- run.sh
+++ run.sh
@@ -1,17 +1,19 @@
 source scripts/sac_exp.sh HalfCheetah-v4 &&
-source scripts/sac_exp.sh Ant-v4 &&
-source scripts/sac_exp.sh Walker2d-v3 &&
-source scripts/sac_exp.sh Hopper-v4 &&
-source scripts/sac_exp.sh Humanoid-v4 &&
-
 source scripts/td3_exp.sh HalfCheetah-v4 &&
-source scripts/td3_exp.sh Ant-v4 &&
-source scripts/td3_exp.sh Walker2d-v3 &&
-source scripts/td3_exp.sh Hopper-v4 &&
-source scripts/td3_exp.sh Humanoid-v4 &&
-
 source scripts/td7_exp.sh HalfCheetah-v4 &&
+
+source scripts/sac_exp.sh Ant-v4 &&
+source scripts/td3_exp.sh Ant-v4 &&
 source scripts/td7_exp.sh Ant-v4 &&
-source scripts/td7_exp.sh Walker2d-v3 &&
+
+source scripts/sac_exp.sh Walker2d-v4 &&
+source scripts/td3_exp.sh Walker2d-v4 &&
+source scripts/td7_exp.sh Walker2d-v4 &&
+
+source scripts/sac_exp.sh Hopper-v4 &&
+source scripts/td3_exp.sh Hopper-v4 &&
 source scripts/td7_exp.sh Hopper-v4 &&
+
+source scripts/sac_exp.sh Humanoid-v4 &&
+source scripts/td3_exp.sh Humanoid-v4 &&
 source scripts/td7_exp.sh Humanoid-v4 &&
